1ef0687348d8b8b50a377f677e8f307479cc7dd6
Iven022 <ivenmoonienxd@gmail.com>
2020-09-20 07:35:29 +0400

    [master] V1

---
+++ bin/check.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+while true;
+do
+
+	cd ~/src
+	dir=$(pwd)
+
+	xls2csv $dir/Embauche/list.xls > $dir/Embauche/bin/converted.csv
+
+	input="$dir/Embauche/bin/converted.csv"
+
+	array=($(awk -F: '{ print $1 }' /etc/passwd ))
+	i=0;
+	while IFS= read -r line
+	do
+		i=0
+		for item in ${array[*]}
+		do
+
+		
+	    		if [ "$line" = "\"$item\"" ]; then
+	 			i=$((i+1))
+			fi
+		done
+		if [ "$i" = 0 ]; then
+			var="$(echo $line | sed 's/\"//g')"
+			useradd $var
+			mkhomedir_helper $var
+		fi	
+	done < "$input"
+done
+
+
+
---
+++ bin/install.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+#checking if ~/src already exists and if not create it.
+if [[ -d ~/src ]] 
+then
+	echo "No need to create the src folder, you already have one."
+else 
+	mkdir ~/src 	
+fi
+
+#going to the src folder and checking if Embauche has already been clone there and if not, clone it.
+cd ~/src 
+if [[ -d ~/src/Embauche]]
+then
+	echo "No need to clone the Embauche repository in the src folder, you already have it."
+else 
+	git clone --single-branch --branch master https://github.com/Iven022/Embauche.git
+fi
+
+cd ~/bin
+cp ~/src/Embauche/bin/check.sh ~/bin/check.sh
+chmod +x check.sh #giving it executable rights
+
+
+cd /etc/systemd/system
+cp ~/src/Embauche/bin/check.service /etc/systemd/system/check.service
+chmod +x check.service #giving it executable rights
+
+sudo systemctl start check.service
+ 
+sudo systemctl enable check.service
+
+echo -e " \e[5mInstallation Completed \e[25m!"
